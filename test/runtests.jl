d488229ba5180bc936170d9051d3bf2beea85ba9
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-04-27 19:10:49 +0100

    add missing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,23 @@ using Test
 
     @testset "isapprox" begin
         @testset "Composite{Tuple}" begin
-            x_tup = (1.5, 2.5, 3.5)
-            x_comp = Composite{typeof(x_tup)}(x_tup...)
-            @test x_comp ≈ x_tup
-            @test x_tup ≈ x_comp
-            @test x_comp ≈ x_comp
+            @testset "basic" begin
+                x_tup = (1.5, 2.5, 3.5)
+                x_comp = Composite{typeof(x_tup)}(x_tup...)
+                @test x_comp ≈ x_tup
+                @test x_tup ≈ x_comp
+                @test x_comp ≈ x_comp
 
-            @test_throws Exception x_comp ≈  collect(x_tup)
+                @test_throws Exception x_comp ≈  collect(x_tup)
+            end
+
+            @testset "different types" begin
+                # both of these are reasonable diffentials for the `Tuple{Int, Int}` primal
+                @test Composite{Tuple{Int, Int}}(1f0, 2f0) ≈ Composite{Tuple{Int, Int}}(1.0, 2.0)
+
+                # But these have different primals so should not be equal
+                @test Composite{Tuple{Int, Int}}(1.0, 2.0) ≈ Composite{Tuple{Float64, Float64}}(1.0, 2.0)
+            end
         end
     end
 
