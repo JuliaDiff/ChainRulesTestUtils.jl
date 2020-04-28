09cd012ae94a2ca75408ecc4dffe4888a4c43fa3
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-04-28 11:27:52 +0100

    fix include

--- src/ChainRulesTestUtils.jl
+++ src/ChainRulesTestUtils.jl
@@ -15,5 +15,5 @@ export test_scalar, frule_test, rrule_test, generate_well_conditioned_matrix
 include("to_vec.jl")
 include("isapprox.jl")
 include("data_generation.jl")
-include("testers,jl")
+include("testers.jl")
 end # module
