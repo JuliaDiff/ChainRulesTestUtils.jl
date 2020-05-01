af68a94a617cd5d907a5b5c350b1201de6062dee
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-05-01 09:24:47 -0500

    Updated testing for well conditioned matrices

--- test/data_generation.jl
+++ test/data_generation.jl
@@ -1,18 +1,23 @@
 @testset "Data Generation" begin
+    function _is_well_conditioned(matrix::Array{Float64})
+        @test !isempty(matrix)
+        @test isposdef(matrix)
+        @test cond(matrix) < 20
+    end
+
+
     @testset "Generate Well Conditioned Matrix" begin
         @testset "Pass in RNG" begin
             rng = MersenneTwister(1)
             matrix = generate_well_conditioned_matrix(rng, 5)
 
-            @test !isempty(matrix)
-            @test isposdef(matrix)
+            _is_well_conditioned(matrix)
         end
 
         @testset "Global RNG" begin
             matrix = generate_well_conditioned_matrix(5)
 
-            @test !isempty(matrix)
-            @test isposdef(matrix)
+            _is_well_conditioned(matrix)
         end
     end
 end
