a3f7221e1d6db549f90d3b43b0c3cec18cf7f23f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-11 09:17:54 +0100

    Set max_range of _fdm to 1e-2

--- src/ChainRulesTestUtils.jl
+++ src/ChainRulesTestUtils.jl
@@ -9,7 +9,7 @@ using LinearAlgebra
 using Random
 using Test
 
-const _fdm = central_fdm(5, 1)
+const _fdm = central_fdm(5, 1; max_range=1e-2)
 
 export TestIterator
 export check_equal, test_scalar, frule_test, rrule_test, generate_well_conditioned_matrix
