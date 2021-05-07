653ae9880ed8cb97a3a7b9be5a7b2a9fa5e18651
Miha Zgubic <mzgubic@users.noreply.github.com>
2021-05-07 10:40:27 +0100

    remove `rand_tangent` from ChainRulesTestUtils and use the one from FiniteDifferences (#147)

--- src/generate_tangent.jl
+++ src/generate_tangent.jl
@@ -3,7 +3,7 @@
 
 Use this in the place of a tangent/cotangent in [`test_frule`](@ref) or
 [`test_rrule`](@ref) to have that tangent/cotangent generated automatically based on the
-primal. Uses [`rand_tangent`](@ref)
+primal. Uses `FiniteDifferences.rand_tangent`.
 """
 struct Auto end
 
@@ -38,47 +38,3 @@ This function is idempotent. If you pass it a `PrimalAndTangent` it doesn't chan
 """
 auto_primal_and_tangent(primal; rng=Random.GLOBAL_RNG) = primal ⊢ rand_tangent(rng, primal)
 auto_primal_and_tangent(both::PrimalAndTangent; kwargs...) = both
-
-"""
-    rand_tangent([rng::AbstractRNG,] x)
-
-Returns a randomly generated tangent vector appropriate for the primal value `x`.
-"""
-rand_tangent(x) = rand_tangent(Random.GLOBAL_RNG, x)
-
-function rand_tangent(rng::AbstractRNG, x::Union{Symbol, AbstractChar, AbstractString})
-    return DoesNotExist()
-end
-
-rand_tangent(rng::AbstractRNG, x::Integer) = DoesNotExist()
-
-rand_tangent(rng::AbstractRNG, x::T) where {T<:Number} = randn(rng, T)
-
-# ref: https://github.com/JuliaLang/julia/issues/17629
-rand_tangent(rng::AbstractRNG, ::BigFloat) = big(randn(rng))
-
-rand_tangent(rng::AbstractRNG, x::StridedArray) = rand_tangent.(Ref(rng), x)
-
-function rand_tangent(rng::AbstractRNG, x::T) where {T<:Tuple}
-    return Composite{T}(rand_tangent.(Ref(rng), x)...)
-end
-
-function rand_tangent(rng::AbstractRNG, xs::T) where {T<:NamedTuple}
-    return Composite{T}(; map(x -> rand_tangent(rng, x), xs)...)
-end
-
-function rand_tangent(rng::AbstractRNG, x::T) where {T}
-    if !isstructtype(T)
-        throw(ArgumentError("Non-struct types are not supported by this fallback."))
-    end
-
-    field_names = fieldnames(T)
-    if length(field_names) > 0
-        tangents = map(field_names) do field_name
-            rand_tangent(rng, getfield(x, field_name))
-        end
-        return Composite{T}(; NamedTuple{field_names}(tangents)...)
-    else
-        return NO_FIELDS
-    end
-end
