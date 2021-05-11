aafdd8091da08b04c15cf0f1fd9c9d7590ec5d0d
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-05-11 10:39:12 +0100

    copy old test

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -23,6 +23,25 @@
     end
 end
 
+@testset "ignoring arguments" begin
+    fsymtest(x, s::Symbol) = x
+    ChainRulesCore.frule((_, Δx, _), ::typeof(fsymtest), x, s) = (x, Δx)
+    function ChainRulesCore.rrule(::typeof(fsymtest), x, s)
+        function fsymtest_pullback(Δx)
+            return NO_FIELDS, Δx, DoesNotExist()
+        end
+        return x, fsymtest_pullback
+    end
+
+    @testset "test_frule" begin
+        test_frule(fsymtest, 2.5, :x ⊢ nothing)
+    end
+
+    @testset "test_rrule" begin
+        test_rrule(fsymtest, 2.5, :x ⊢ nothing)
+    end
+end
+
 @testset "old testers.jl" begin
     @testset "unary: identity(x)" begin
         function ChainRulesCore.frule((_, ẏ), ::typeof(identity), x)
