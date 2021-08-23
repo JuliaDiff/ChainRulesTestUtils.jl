f197a3106272868f87a34ff261a5b7dc425988a2
jieli-matrix <li_j20@fudan.edu.cn>
2021-08-23 20:17:10 +0800

    all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,10 +11,10 @@ using Test
 ChainRulesTestUtils.TEST_INFERRED[] = true
 
 @testset "ChainRulesTestUtils.jl" begin
-    # include("meta_testing_tools.jl")
-    # include("iterator.jl")
-    # include("check_result.jl")
-    # include("testers.jl")
-    # include("data_generation.jl")
+    include("meta_testing_tools.jl")
+    include("iterator.jl")
+    include("check_result.jl")
+    include("testers.jl")
+    include("data_generation.jl")
     include("rand_tangent.jl")
 end
