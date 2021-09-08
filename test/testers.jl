1bfee84cd14cd0d78590c0e3bf7d307fd60395f8
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-09-08 11:43:13 +0100

    fix typo

--- test/testers.jl
+++ test/testers.jl
@@ -723,6 +723,6 @@ end
 
     @testset "inference of thunked cotangents" begin
         @test errors(() -> test_rrule(my_id, 2.0))
-        test_rrule(my_id, 2.0; check_inference=false)
+        test_rrule(my_id, 2.0; check_inferred=false)
     end
 end
