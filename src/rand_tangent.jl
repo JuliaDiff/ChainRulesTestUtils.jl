faef826035e968eeef740a1c9ac344cccad23217
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-11-16 13:10:07 +0000

    Simply rand_tangent(::AbstractArray)

--- src/rand_tangent.jl
+++ src/rand_tangent.jl
@@ -22,19 +22,7 @@ end
 # multiply by 9 to give a bigger range of values tested: no so tightly clustered around 0.
 rand_tangent(rng::AbstractRNG, ::BigFloat) = round(big(9 * randn(rng)), digits=5, base=2)
 
-rand_tangent(rng::AbstractRNG, x::Array{<:Any, 0}) = _compress_notangent(fill(rand_tangent(rng, x[])))
-rand_tangent(rng::AbstractRNG, x::Array) = _compress_notangent(rand_tangent.(Ref(rng), x))
-
-# All other AbstractArray's can be handled using the ProjectTo mechanics.
-# and follow the same requirements
-function rand_tangent(rng::AbstractRNG, x::AbstractArray)
-    return _compress_notangent(ProjectTo(x)(rand_tangent(rng, collect(x))))
-end
-
-# TODO: arguably ProjectTo should handle this for us for AbstactArrays
-# https://github.com/JuliaDiff/ChainRulesCore.jl/issues/410
-_compress_notangent(::AbstractArray{NoTangent}) = NoTangent()
-_compress_notangent(x) = x
+rand_tangent(rng::AbstractRNG, x::AbstractArray) = ProjectTo(x)(rand_tangent.(Ref(rng), x))
 
 function rand_tangent(rng::AbstractRNG, x::T) where {T}
     if !isstructtype(T)
