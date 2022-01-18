d83ec0e91ac6b7d283df7c3b4c937ce6fb271ebe
Lyndon White <lyndon.white@invenialabs.co.uk>
2022-01-18 14:07:39 +0000

    Skip tests that require delete_method on older julia versions

--- test/global_checks.jl
+++ test/global_checks.jl
@@ -2,28 +2,33 @@ struct BadConstructor
     x
 end
 
-@testset "global_checks.jl" begin
-    test_method_tables_sensibility()
+if VERSION >= v"1.3"
+    @testset "global_checks.jl" begin
+        test_method_tables_sensibility()
+        ChainRulesCore.rrule(::typeof(BadConstructor), x) = nothing
+        @test fails(test_method_tables_sensibility)
+        Base.delete_method(Base.which(rrule, (DataType, Any)))
+        test_method_tables_sensibility()  # make sure delete worked
 
-    ChainRulesCore.rrule(::typeof(BadConstructor), x) = nothing
-    @test fails(test_method_tables_sensibility)
-    Base.delete_method(Base.which(Tuple{typeof(rrule), DataType, Any}))
-    test_method_tables_sensibility()  # make sure delete worked
-
-    ChainRulesCore.rrule(::RuleConfig, ::typeof(BadConstructor), x) = nothing
-    @test fails(test_method_tables_sensibility)
-    Base.delete_method(Base.which(Tuple{typeof(rrule), RuleConfig, DataType, Any}))
-    test_method_tables_sensibility()  # make sure delete worked
+        ChainRulesCore.rrule(::RuleConfig, ::typeof(BadConstructor), x) = nothing
+        @test fails(test_method_tables_sensibility)
+        Base.delete_method(Base.which(rrule, (RuleConfig, DataType, Any)))
+        test_method_tables_sensibility()  # make sure delete worked
 
 
 
-    ChainRulesCore.frule(::Any, ::typeof(BadConstructor), x) = nothing
-    @test fails(test_method_tables_sensibility)
-    Base.delete_method(Base.which(Tuple{typeof(frule), Any, DataType, Any}))
-    test_method_tables_sensibility()  # make sure delete worked
+        ChainRulesCore.frule(::Any, ::typeof(BadConstructor), x) = nothing
+        @test fails(test_method_tables_sensibility)
+        Base.delete_method(Base.which(frule, (Any, DataType, Any)))
+        test_method_tables_sensibility()  # make sure delete worked
 
-    ChainRulesCore.frule(::RuleConfig, ::Any, ::typeof(BadConstructor), x) = nothing
-    @test fails(test_method_tables_sensibility)
-    Base.delete_method(Base.which(Tuple{typeof(frule), RuleConfig, Any, DataType, Any}))
-    test_method_tables_sensibility()  # make sure delete worked
+        ChainRulesCore.frule(::RuleConfig, ::Any, ::typeof(BadConstructor), x) = nothing
+        @test fails(test_method_tables_sensibility)
+        Base.delete_method(Base.which(frule, (RuleConfig, Any, DataType, Any)))
+        test_method_tables_sensibility()  # make sure delete worked
+    end
+else # pre 1.3, so no `delete_method` so just test happy path
+    @testset "global_checks.jl" begin
+        test_method_tables_sensibility()
+    end
 end
